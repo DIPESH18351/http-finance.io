28f2a58b3dbd0ceadb285a63179904d415a40f80
DIPESH18351 <gargdipesh41@gmail.com>
2026-05-17 17:43:53 +0530

    Updated backend URL

--- client/src/axios-instance.ts
+++ client/src/axios-instance.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const backend = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BASE_URL || "https://http-finance-io.onrender.com",
   timeout: 60000,
   headers: { "X-Custom-Header": "foobar" },
 });
